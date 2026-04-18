13e2d7597398d33deb44b0b3e18625d309161c9c
PSTY2 <_@example.com>
2026-04-18 18:38:45 +0300

    ci: full CD pipeline

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const statusBox = document.getElementById('status');
 const checkBtn = document.getElementById('check-btn');
-const unusedVariable = 'я ніде не використовуюсь';
 
 function getStatus() {
   const now = new Date();
   const timeString = now.toLocaleTimeString('uk-UA');
-
-  if (timeString == null) {
-    return;
-  }
 
   statusBox.textContent = `Все працює. Перевірено о ${timeString}`;
 }
